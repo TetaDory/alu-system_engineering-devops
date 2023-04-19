4eb1e94641b2d565090f98c30d5dd7005e7a9479
TetaDory <d.rutayisir1@alustudent.com>
2023-04-19 17:02:54 +0000

    d

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -5,4 +5,3 @@ exec { 'replace_hard_soft_limit_for_holberton_user':
   command => 'sed -i "/holberton hard nofile/c\holberton hard nofile 4096" /etc/security/limits.conf;\
 sed -i "/holberton soft nofile/c\holberton soft nofile 1024" /etc/security/limits.conf',
 }
-
